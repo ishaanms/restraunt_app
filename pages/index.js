62383a6a505d2b5dc043bfee300fd4dfd809d8d0
ishaanms <78543481+ishaanms@users.noreply.github.com>
2022-08-18 00:41:52 +0530

    second

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import axios from 'axios'
+import Image from 'next/image'
+import Featured from '../components/Featured'
+import PizzaList from '../components/PizzaList'
+import styles from '../styles/Home.module.css'
+
+export default function Home({pizzaList}) {
+  return (
+    <div className={styles.container}>
+      
+      <Head>
+        <title>Pizzeria</title>
+        <meta name="description" content="Best pizza shop in town!!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Featured/>
+      <PizzaList pizzaList={pizzaList}/>
+    </div>
+  );
+}
+export const getServerSideProps= async()=>{
+  const res = await axios.get("/api/products");
+  return {
+    props: {
+      pizzaList: res.data,
+      // admin,
+    },
+  };
+};
---
+++ util/mongo.js
@@ -0,0 +1,5 @@
+import mongoose from 'mongoose';
+
+const dbConnect= async () => mongoose.connect(process.env.MONGO_URL);
+
+export default dbConnect;
